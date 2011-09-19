fc8dacd6401f2da609e08fae5ccfb4a8d18e0a5f
Karl Gusner <karlgusner@gmail.com>
2011-09-18 20:43:45 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eazybuzz::Application.config.secret_token = '10d00b703878932e2b13710310f94e055888c87e2b348ea34cfb4541990a116badc2307c94668d30e0c699d21c4f7416b0f8d1fc06f3696a6a4a8a693d66787d'
